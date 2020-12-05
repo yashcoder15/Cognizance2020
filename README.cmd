91cfe74aeea4395437eff99e6a79c6cc418428b2
yashcoder15 <75330778+yashcoder15@users.noreply.github.com>
2020-12-05 23:27:43 +0530

    Task-3

---
+++ README.cmd
@@ -0,0 +1,35 @@
+# Cognizance2020
+Getting familiar with Git operations
+Basic Operations
+The following is a summary of basic git operations:
+
+git add
+Puts current working files into the stage.
+git checkout
+Replaces the current working files with files from a branch.
+git checkout -b
+Creates a new local branch from the current branch's tip.
+git clone
+Clone an existing repository into a new directory.
+git commit
+Commits staged changes to a local branch.
+git commit -a
+Commits all modified files to a local branch (shorthand for "git add" followed by 
+"git commit" for each modified file)
+git fetch
+Downloads changes from a remote repository into the local clone.
+git merge
+Merges files from a given branch into the current branch.
+git pull
+Fetches remote changes on the current branch into the local clone, and merges 
+them into the current working files.
+git push
+Uploads changes from all local branches to the respective remote repositories.
+git reset
+Makes the current branch point to some specific revision or branch
+git rebase
+Integrates changes from one branch into another. It is an alternative to the 
+better known "merge" command.
+git fork
+Fork will create a copy of the repository in your GitHub account so that you 
+can make changes to the project.
